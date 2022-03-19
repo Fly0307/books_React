26da1924a0263efcaa28c5f5fdc55038b8361b22
fly0307 <AlbertXi1201@gmail.com>
2022-03-19 09:54:38 +0800

    0.0

---
+++ src/js/books.js
@@ -0,0 +1,14 @@
+class book {
+    name;
+    auth;
+    unitPrice;
+    imageUrl;
+}
+
+function getBooks() {
+    return
+}
+
+function refreshBooks() {
+    document.getElementById("allBooks").innerHTML = {};
+}
